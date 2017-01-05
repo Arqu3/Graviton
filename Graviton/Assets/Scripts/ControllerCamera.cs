4fcfe413489412b7b1667723360df308cb380e5e
Ludwig Gustavsson <ludwiggustavsson3@gmail.com>
2017-01-05 01:25:49 +0100

    Added all the basics

---
+++ Graviton/Assets/Scripts/ControllerCamera.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera), typeof(Transform))]
+public class ControllerCamera : MonoBehaviour
+{
+    //Public vars
+    public bool m_InvertY = true;
+    public Vector2 m_Sensitivity = new Vector2(3, 1);
+    public float m_YClamp = 70.0f;
+
+    //Component vars
+    ControllerPlayer m_Player;
+
+    //Rotation var
+    private int m_InvertValue = 1;
+    private Vector2 m_Inputs = new Vector2(0, 0);
+    private float m_AbsoluteX = 0.0f;
+    private float m_AbsoluteY = 0.0f;
+
+	void Start ()
+    {
+        m_Player = FindObjectOfType<ControllerPlayer>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+	
+	void Update ()
+    {
+        RotationUpdate();
+	}
+
+    void RotationUpdate()
+    {
+        m_InvertValue = Mathf.Clamp(m_InvertValue, -1, 1);
+        if (m_InvertY)
+            m_InvertValue = -1;
+        else
+            m_InvertValue = 1;
+
+        m_Inputs = new Vector2(Input.GetAxis("Mouse X") * m_Sensitivity.x, Input.GetAxis("Mouse Y") * m_Sensitivity.y * m_InvertValue);
+
+        m_AbsoluteX += m_Inputs.x;
+        if (m_AbsoluteX >= 360)
+            m_AbsoluteX -= 360;
+        else if (m_AbsoluteX <= -360)
+            m_AbsoluteX += 360;
+
+        m_AbsoluteY += m_Inputs.y;
+        m_AbsoluteY = Mathf.Clamp(m_AbsoluteY, -m_YClamp, m_YClamp);
+
+        transform.rotation = Quaternion.Euler(m_AbsoluteY, m_Player.transform.rotation.eulerAngles.y, m_Player.transform.rotation.eulerAngles.z);
+        //m_Player.transform.rotation = Quaternion.Euler(m_Player.transform.rotation.eulerAngles.x, m_AbsoluteX, m_Player.transform.rotation.eulerAngles.z);
+        m_Player.transform.rotation = Quaternion.Euler(0, m_AbsoluteX, 0);
+    }
+}
---
+++ Graviton/Assets/Scripts/ControllerPlayer.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(Collider))]
+public class ControllerPlayer : MonoBehaviour
+{
+    //Public vars
+    public float m_MovementSpeed = 20.0f;
+    public float m_JumpForce = 10.0f;
+    public bool m_IsAirMovement = false;
+
+    //Component vars
+    private Collider m_Collider;
+    private Rigidbody m_Rigidbody;
+
+    //Jump vars
+    private bool m_IsGoingUp = false;
+    private bool m_IsOnGround = true;
+
+	void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_Collider = GetComponent<Collider>();
+	}
+	
+	void Update()
+    {
+        if (transform.position.y < -15f)
+            transform.position = new Vector3(0, 5, 0);
+
+        MovementUpdate();
+
+        JumpUpdate();
+	}
+
+    void MovementUpdate()
+    {
+        if (m_Rigidbody.velocity.magnitude < 1.0f && m_IsOnGround)
+            m_Rigidbody.velocity = Vector3.zero;
+
+        if (Raycast(m_Collider.bounds.center, Vector3.down, 1.2f))
+        {
+            m_IsOnGround = true;
+            m_Rigidbody.useGravity = false;
+        }
+        else
+        {
+            m_IsOnGround = false;
+            m_Rigidbody.useGravity = true;
+        }
+
+        Vector3 fwd = Camera.main.transform.forward;
+        fwd.y = 0.0f;
+        Quaternion rot = Quaternion.LookRotation(fwd);
+
+        if (m_IsAirMovement)
+            m_Rigidbody.velocity = rot * new Vector3(Input.GetAxis("Horizontal") * m_MovementSpeed, m_Rigidbody.velocity.y, Input.GetAxis("Vertical") * m_MovementSpeed);
+        else
+        {
+            if (m_IsOnGround)
+                m_Rigidbody.velocity = rot * new Vector3(Input.GetAxis("Horizontal") * m_MovementSpeed, m_Rigidbody.velocity.y, Input.GetAxis("Vertical") * m_MovementSpeed);
+        }
+    }
+
+    void JumpUpdate()
+    {
+        m_IsGoingUp = Mathf.Round(m_Rigidbody.velocity.y) > 0;
+
+        if (m_IsOnGround && !m_IsGoingUp)
+        {
+            if (Input.GetKey(KeyCode.Space))
+                m_Rigidbody.velocity = new Vector3(m_Rigidbody.velocity.x, m_JumpForce, m_Rigidbody.velocity.z);
+        }
+    }
+
+    /// <summary>
+    /// Raycasts from given position with given direction to distance, and also draws the ray.
+    /// The rays base-color is green, if it hits something it changes to red.
+    /// </summary>
+    /// <param name="position"></param>
+    /// <param name="direction"></param>
+    /// <param name="distance"></param>
+    /// <returns></returns>
+    bool Raycast(Vector3 position, Vector3 direction, float distance)
+    {
+        bool hit = false;
+        Color hitCol = Color.green;
+
+        RaycastHit rayhit;
+        hit = Physics.Raycast(position, direction, out rayhit, distance);
+        if (hit)
+            hitCol = Color.red;
+
+        Debug.DrawRay(position, direction * distance, hitCol);
+
+        return hit;
+    }
+}
